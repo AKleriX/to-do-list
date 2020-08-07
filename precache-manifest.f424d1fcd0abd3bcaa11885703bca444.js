a3ab9d0db2016970364ab873534b63238f3ff45c
AKleriX <AKleriX@users.noreply.github.com>
2020-08-07 19:57:44 +0300

    Updates

---
+++ precache-manifest.f424d1fcd0abd3bcaa11885703bca444.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6632e8b14fb5c4ffe58d1994dd77317c",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "f3ea9e8a3d68a08ee87e",
+    "url": "/to-do-list/static/css/main.3487f402.chunk.css"
+  },
+  {
+    "revision": "b58c40ff1409454ec2fe",
+    "url": "/to-do-list/static/js/2.99e6b070.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/to-do-list/static/js/2.99e6b070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ea9e8a3d68a08ee87e",
+    "url": "/to-do-list/static/js/main.8ff09f6c.chunk.js"
+  },
+  {
+    "revision": "1c7e5237723f2e4242a1",
+    "url": "/to-do-list/static/js/runtime-main.d622a4a1.js"
+  },
+  {
+    "revision": "4c83d11ca997f41f2b7641d0fd4a8309",
+    "url": "/to-do-list/static/media/add.4c83d11c.svg"
+  },
+  {
+    "revision": "20ced97c9d8dfacb9fd104246be8dde6",
+    "url": "/to-do-list/static/media/close.20ced97c.svg"
+  },
+  {
+    "revision": "5c12591d6605d7bf92b16919c3736e95",
+    "url": "/to-do-list/static/media/edit.5c12591d.svg"
+  },
+  {
+    "revision": "73cfd7f84d21a751bd819d89749f4357",
+    "url": "/to-do-list/static/media/loader.73cfd7f8.svg"
+  },
+  {
+    "revision": "665926e22b76aa6339da4f7eab12c067",
+    "url": "/to-do-list/static/media/trash.665926e2.svg"
+  }
+]);
